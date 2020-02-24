097ddd95b6f3bf65fc03fbd3673b4816b43e7d54
ghostsf <ghost_sf@163.com>
2020-02-24 18:58:22 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+window.FastClick = require('fastclick');
+require('lazyload');
+
+window.notie = require('corner-notie');
+window.APlayer = require('aplayer');
+window.DPlayer = require('dplayer');
+
+require('./utils');
+require('./motion');
+require('./affix');
+require('./pisces')();
+require('./scrollspy');
+require('./post-details')();
+require('./bootstrap');
+require('./evanyou');
+require('./leancloud')();
+require('./share')();
+require('./scroll');
+require('./since');
+require('./title');
+require('./type');
+// require('./kanban');
+require('./mix');
+require('./clipboard');
+require('./pjax');
+require('./online');
+require('./search');
+require('./zoom')();
+require('./aplayer');
+require('./cursor-effects');
